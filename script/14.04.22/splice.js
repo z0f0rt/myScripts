9041057e114d2adf7e290d26ad35b60ab620d1fb
z0f0rt <greenboots@bk.ru>
2022-04-14 14:35:39 +0300

    практика slice,splice,concat

--- script/14.04.22/splice.js
+++ script/14.04.22/splice.js
@@ -1,7 +1,11 @@
 let numbers = [12, 23, 45, 56, 78, 89];
 numbers.splice(0, 2, "Rather", "Exam");
-console.log(numbers);
+console.log(`Изменённый оригинальный массив через splice: ${numbers}`);
 
 let appleZIGZAG = ["Катя", "Ваня", "Саша", "Маша", "Дима"];
-let newappleZIGZAG = appleZIGZAG.slice();
-console.log(newappleZIGZAG);
+let newappleZIGZAG = appleZIGZAG.slice(1, 4);
+console.log("Новый изменённый масcив урезанный через slice: " + newappleZIGZAG);
+console.log("Оригинальный неизменённый массив: " + appleZIGZAG);
+
+let contactAZZ = appleZIGZAG.concat("Володя");
+console.log("Изменённый масиив через concat с Володей: " + contactAZZ);
