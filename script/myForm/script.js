3eb0b406ed00883836c4afec37fb0cb09e12df99
z0f0rt <greenboots@bk.ru>
2022-04-28 23:26:00 +0300

    update #2

--- script/myForm/script.js
+++ script/myForm/script.js
@@ -2,8 +2,18 @@ document.querySelector("button").addEventListener("click", () => {
   let inputs = document.querySelectorAll("input");
   let obj = {
     name: inputs[0].value,
-    password: inputs[1].value
+    password: inputs[1].value,
   };
-  // alert(`Имя пользователя: ${obj.name}, Пароль: ${obj.password}`)
+  if( inputs[0].value === 'Слава'){
+    document.querySelector('.suk').innerHTML = "Пользователь с таким именем уже зарегестрирован!"
+  }else{
   console.log(`Имя пользователя: ${obj.name}, Пароль: ${obj.password}`);
-});
+
+  document.querySelector('.sum').innerHTML = `Пользователь с Именем: ${obj.name} и Паролем: ${obj.password} успешно зарегестрирован. Поздравляю!`
+}});
+
+// document.querySelector("button").onclick = () => {
+//   alert("Вы нажали кнопку");
+// };
+
+// alert(`Имя пользователя: ${obj.name}, Пароль: ${obj.password}`)
