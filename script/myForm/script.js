3696e539ffc6cad75e44c72da698dfb693bbcc47
z0f0rt <greenboots@bk.ru>
2022-04-29 01:52:24 +0300

    upload whith new task #3

--- script/myForm/script.js
+++ script/myForm/script.js
@@ -4,13 +4,17 @@ document.querySelector("button").addEventListener("click", () => {
     name: inputs[0].value,
     password: inputs[1].value,
   };
-  if( inputs[0].value === 'Слава'){
-    document.querySelector('.suk').innerHTML = "Пользователь с таким именем уже зарегестрирован!"
-  }else{
-  console.log(`Имя пользователя: ${obj.name}, Пароль: ${obj.password}`);
-
-  document.querySelector('.sum').innerHTML = `Пользователь с Именем: ${obj.name} и Паролем: ${obj.password} успешно зарегестрирован. Поздравляю!`
-}});
+  let x = document.querySelector("#id-sum");
+  if (inputs[0].value === "Слава") {
+    // document.querySelector('.suk').innerHTML = "Пользователь с таким именем уже зарегестрирован!"
+    x.className = "suk";
+    x.innerHTML = "Пользователь с таким именем уже зарегестрирован!";
+  } else {
+    console.log(`Имя пользователя: ${obj.name}, Пароль: ${obj.password}`);
+    x.className = "sum";
+    x.innerHTML = `Пользователь с именем: ${obj.name} и паролем: ${obj.password} успешно зарегестрирован. Поздравляю!`;
+  }
+});
 
 // document.querySelector("button").onclick = () => {
 //   alert("Вы нажали кнопку");
