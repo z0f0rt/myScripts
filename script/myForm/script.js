febdaaa57855778b8c20f2d98c576af648a25d52
z0f0rt <greenboots@bk.ru>
2022-05-05 03:16:38 +0300

    figma registration form #1

--- script/myForm/script.js
+++ script/myForm/script.js
@@ -3,7 +3,7 @@ document.querySelector("button").addEventListener("click", () => {
   let obj = {
     name: inputs[0].value,
     password: inputs[1].value,
-  };
+  };;
   let x = document.querySelector("#id-sum");
   if (inputs[0].value === "Слава") {
     // document.querySelector('.suk').innerHTML = "Пользователь с таким именем уже зарегестрирован!"
