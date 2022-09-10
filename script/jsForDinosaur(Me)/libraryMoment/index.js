b20158e673ba8ca42b747f042ec5aa6808722e08
z0f0rt <greenboots@bk.ru>
2022-09-10 04:41:14 +0300

    bundle?

--- script/jsForDinosaur(Me)/libraryMoment/index.js
+++ script/jsForDinosaur(Me)/libraryMoment/index.js
@@ -1,3 +1,4 @@
+'use strict'
+var moment = require("moment");
 console.log("Hello from JavaScript!");
 console.log(moment().startOf("day").fromNow());
-alert(moment().format("MMMM Do YYYY, h:mm:ss a"));
