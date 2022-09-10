987229d2a286cb2b8f1108d74736d094ad796801
z0f0rt <greenboots@bk.ru>
2022-09-11 00:17:08 +0300

    Знакомство с технологиями

--- script/jsForDinosaur(Me)/libraryMoment/index.js
+++ script/jsForDinosaur(Me)/libraryMoment/index.js
@@ -1,4 +1,6 @@
-'use strict'
-var moment = require("moment");
+import moment from "moment";
 console.log("Hello from JavaScript!");
 console.log(moment().startOf("day").fromNow());
+var name = "Slava",
+  time = "today";
+console.log(`Hello ${name}, how dare are you ${time}?`);
