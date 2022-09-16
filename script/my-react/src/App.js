b8ade39b6afa40816a8e761d65c9ffdd899813f9
z0f0rt <greenboots@bk.ru>
2022-09-16 17:26:43 +0300

    calc

--- script/my-react/src/App.js
+++ script/my-react/src/App.js
@@ -2,45 +2,49 @@ import React, { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0);
-  const kek = 0;
-
+  const [notification, setNotification] = useState("");
   function increment() {
     setCount(count + 1);
-    setCount(count + 1);
+    setNotification();
   }
 
   function decrement() {
     setCount(count - 1);
+    setNotification();
   }
 
   function multiply2() {
     setCount(count * 2);
+    setNotification();
   }
 
   function root() {
-    if (count > 0) {
+    if (count >= 0) {
       setCount(Math.sqrt(count));
+      setNotification();
     } else {
-      alert("Ошибка");
+      setNotification("Ошибка");
+      // alert("Ошибка, нельзя извлекать корень из отрицательного числа");
     }
   }
-
   function clear() {
-    setCount(count * kek);
+    setCount(0);
+    setNotification();
   }
   function square() {
     setCount(count ** 2);
+    setNotification();
   }
   return (
     <div className="App">
       <h1>{count}</h1>
-      <input value={count}></input>
       <button onClick={increment}>+</button>
       <button onClick={decrement}>-</button>
       <button onClick={multiply2}>x2</button>
       <button onClick={root}>&radic;</button>
       <button onClick={square}>square</button>
       <button onClick={clear}>clear</button>
+      <h1>{notification}</h1>
     </div>
   );
 }
