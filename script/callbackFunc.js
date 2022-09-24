9a810ba910fe1fe3ebeb7881f2d9dad24a8f13b9
z0f0rt <greenboots@bk.ru>
2022-09-24 10:49:05 +0300

    callbackFunc

---
+++ script/callbackFunc.js
@@ -0,0 +1,18 @@
+// "use strict";
+// let arr = [2, 4, 6, 8];
+// let newArr = [];
+// function cicle(array, callback) {
+//   for (let i = 0; i < array.length; i++) {
+//     let variable = callback(array[i]);
+//     newArr.push(variable);
+//   }
+//   console.log(newArr);
+// }
+
+// function kek(num) {
+//   return (num = num + 2);
+// }
+// cicle(arr, kek);
+
+let arr = [3, 2, 1, 2, 5, 6];
+console.log(arr.reverse());
