90f5852664ec68bf62d4df634c41c084e975e550
z0f0rt <greenboots@bk.ru>
2022-10-14 23:36:33 +0300

    add discriptions

--- script/reactmeeting2/src/App.js
+++ script/reactmeeting2/src/App.js
@@ -8,10 +8,10 @@ import MyInput from "./components/UI/Input/MyInput";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript 1", body: "Description" },
-    { id: 2, title: "JavaScript 2", body: "Description" },
-    { id: 3, title: "JavaScript 3", body: "Description" },
-    { id: 4, title: "JavaScript 4", body: "Description" },
+    { id: 1, title: "JavaScript 1", body: "Description1" },
+    { id: 2, title: "JavaScript 2", body: "Description2" },
+    { id: 3, title: "JavaScript 3", body: "Description3" },
+    { id: 4, title: "JavaScript 4", body: "Description4" },
   ]);
 
   const [title, setTitle] = useState("");
