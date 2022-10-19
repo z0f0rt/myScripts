d38c7b818f84578ff36c9bfcf5cfd7b6ad9ea70d
z0f0rt <greenboots@bk.ru>
2022-10-19 23:40:32 +0300

    without Ref

--- script/reactmeeting2/src/components/UI/Input/MyInput.jsx
+++ script/reactmeeting2/src/components/UI/Input/MyInput.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classes from "./MyInput.module.css";
 
-const MyInput = React.forwardRef((props, ref) => {
-  return <input ref={ref} className={classes.myInput} {...props} />;
-});
+const MyInput = (props) => {
+  return <input className={classes.myInput} {...props} />;
+};
 export default MyInput;
