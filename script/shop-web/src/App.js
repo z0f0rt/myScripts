af5efa8caf8e10c6c175594570fe5907d62f9432
z0f0rt <greenboots@bk.ru>
2022-11-20 23:24:47 +0300

    not wrong

--- script/shop-web/src/App.js
+++ script/shop-web/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { useState } from "react";
 import { products } from "./constants/products";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout";
