c101796794c970d6bcb9e483c36cae5c5a5e7c13
z0f0rt <greenboots@bk.ru>
2022-11-21 23:39:09 +0300

    front-back

--- script/backend-express/index.js
+++ script/backend-express/index.js
@@ -1,9 +1,40 @@
 import express from "express";
-
+import cors from "cors";
 const POST = 5000;
 const app = express();
+app.use(cors());
 
+const products = [
+  {
+    name: "Смартфон Samsung Galaxy Z Fold4 512 ГБ бежевый",
+    price: 117565,
+  },
+  {
+    name: "Экшн-камера GoPro HERO 11 Black Edition черный",
+    price: 28699,
+  },
+  {
+    name: '17.3" Ноутбук MSI GT77 Titan 12UHS-066RU черный',
+    price: 177499,
+  },
+  {
+    name: "Дрель-шуруповерт DeWalt XRP DCD996P2 XR18V",
+    price: 8750,
+  },
+  {
+    name: "Вытяжка каминная Kaiser AT 6433 Eco серебристый/черный",
+    price: 15300,
+  },
+  {
+    name: '31.5" Монитор Samsung Odyssey G7 C32G75TQSI черный',
+    price: 45500,
+  },
+];
 
+app.get("/", (req, res) => {
+  res.status(200).json(products);
+});
 
-
-app.listen(POST, ()=>{console.log('Сервер удачно запустился на порте:' + POST)})
+app.listen(POST, () => {
+  console.log(`Сервер удачно запустился на порте:${POST}...`);
+});
--- script/shop-web/src/App.js
+++ script/shop-web/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { products } from "./constants/products";
+// import { products } from "./constants/products";
 import { Routes, Route } from "react-router-dom";
 import { Layout } from "./Layout";
 import { Products } from "./Products";
@@ -9,22 +9,23 @@ import { LINKS } from "./constants/links";
 import { useDispatch } from "react-redux";
 
 function App() {
-  // const [countAll, setCountAll] = useState([]);
   const dispatch = useDispatch();
 
+  function productsFetch(url) {
+    return fetch(url).then((res) => res.json());
+  }
+
   useEffect(() => {
+    const products = productsFetch("http://localhost:5000");
+    products.then((res) => {
+      
+    });
     const countLocal = localStorage.getItem("count");
     const countParse = JSON.parse(countLocal);
     dispatch({
       type: "INIT",
       payload: { initCounts: products.length, local: countParse },
     });
-    // const countAllInit = products.map(() => undefined);
-    // if (countLocal) {
-    //   setCountAll(JSON.parse(countAll));
-    //   return;
-    // }
-    // setCountAll(countAllInit);
   }, []);
 
   return (
