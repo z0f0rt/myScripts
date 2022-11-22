63d389ae307ef7cc53015a45f313c4d5e14d4a8a
z0f0rt <greenboots@bk.ru>
2022-11-22 18:35:49 +0300

    back: add import photos of products

--- script/backend-express/index.js
+++ script/backend-express/index.js
@@ -1,5 +1,12 @@
 import express from "express";
 import cors from "cors";
+import phone from "./public/images/phone.png";
+import camera from "./public/images/camera.png";
+import notebook from "./public/images/notebook.jpg";
+import shurik from "./public/images/shurik.jpg";
+import viteshka from "./public/images/viteshka.jpg";
+import screen from "./public/images/screen.jpg";
+
 const POST = 5000;
 const app = express();
 app.use(cors());
@@ -7,26 +14,32 @@ app.use(cors());
 const products = [
   {
     name: "Смартфон Samsung Galaxy Z Fold4 512 ГБ бежевый",
+    image: phone,
     price: 117565,
   },
   {
     name: "Экшн-камера GoPro HERO 11 Black Edition черный",
+    image: camera,
     price: 28699,
   },
   {
     name: '17.3" Ноутбук MSI GT77 Titan 12UHS-066RU черный',
+    image: notebook,
     price: 177499,
   },
   {
     name: "Дрель-шуруповерт DeWalt XRP DCD996P2 XR18V",
+    image: shurik,
     price: 8750,
   },
   {
     name: "Вытяжка каминная Kaiser AT 6433 Eco серебристый/черный",
+    image: viteshka,
     price: 15300,
   },
   {
     name: '31.5" Монитор Samsung Odyssey G7 C32G75TQSI черный',
+    image: screen,
     price: 45500,
   },
 ];
